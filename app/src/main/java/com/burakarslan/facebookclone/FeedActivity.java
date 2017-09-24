a3363c7557372742bdd04eda8cc4cf3f8ffa427b
Burak <burakarslan5194@gmail.com>
2017-09-24 13:52:14 +0300

    tab bar yapıldı

--- app/src/main/java/com/burakarslan/facebookclone/FeedActivity.java
+++ app/src/main/java/com/burakarslan/facebookclone/FeedActivity.java
@@ -1,13 +1,41 @@
 package com.burakarslan.facebookclone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class FeedActivity extends AppCompatActivity {
+
+    SectionsPageAdapter sectionsPageAdapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+            sectionsPageAdapter=new SectionsPageAdapter(getSupportFragmentManager());
+        viewPager=(ViewPager) findViewById(R.id.container);
+        setupPager(viewPager);
+
+        TabLayout tabLayout=(TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.mipmap.ic_launcher);
+
+            }
+
+
+            private void setupPager(ViewPager viewPager){
+                SectionsPageAdapter adapter=new SectionsPageAdapter(getSupportFragmentManager());
+                adapter.addFragment(new FragmentAnasayfa(),"Anasayfa");
+                adapter.addFragment(new FragmentBildirimler(),"Bildirimler");
+                adapter.addFragment(new FragmentProfil(),"Profil");
+                viewPager.setAdapter(adapter);
+
+            }
     }
-}
+
+
+
+
---
+++ app/src/main/java/com/burakarslan/facebookclone/FragmentAnasayfa.java
@@ -0,0 +1,28 @@
+package com.burakarslan.facebookclone;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Burak on 9/24/2017.
+ */
+
+public class FragmentAnasayfa extends Fragment {
+
+    TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_anasayfa,container,false);
+        textView=(TextView) view.findViewById(R.id.textView);
+
+        return view;
+    }
+}
