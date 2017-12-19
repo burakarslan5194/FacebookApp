8a89333e8328abbd248c5d6a4e414d47a446c44a
Burak <burakarslan5194@gmail.com>
2017-12-19 17:28:45 +0300

    son hali

--- app/src/main/java/com/burakarslan/facebookclone/GroupAddActivity.java
+++ app/src/main/java/com/burakarslan/facebookclone/GroupAddActivity.java
@@ -1,23 +1,17 @@
 package com.burakarslan.facebookclone;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.UUID;
 
@@ -51,8 +45,10 @@ buttongrupadd.setOnClickListener(new View.OnClickListener() {
         String uuidString = uuid.toString();
         FirebaseUser user = mAuth.getCurrentUser();
         String userEmail = user.getEmail().toString();
+        String uyeSayisi="0";
         myRef.child("Groups").child(ad).child("Groupname").setValue(ad);
         myRef.child("Groups").child(ad).child("Yönetici").setValue(userEmail);
+        myRef.child("Groups").child(ad).child("Uye Sayisi").setValue(uyeSayisi);
         Toast.makeText(getApplicationContext(), "Grup Oluşturuldu", Toast.LENGTH_LONG).show();
         Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
         startActivity(intent);
--- app/src/main/java/com/burakarslan/facebookclone/PostClass2.java
+++ app/src/main/java/com/burakarslan/facebookclone/PostClass2.java
@@ -1,7 +1,6 @@
 package com.burakarslan.facebookclone;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,19 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.UUID;
-
-import static com.burakarslan.facebookclone.R.id.imageView2;
 
 /**
  * Created by taner on 12/14/17.
@@ -37,7 +31,7 @@ public class PostClass2 extends ArrayAdapter<String> {
     private FirebaseAuth.AuthStateListener mAuthListener;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference myRef;
-    String s, keyfriend, uuidString, keyuser, friendemailtext;
+    String s, keyfriend, uuidString, keyuser, friendemailtext,swap;
     String emailText, arkadascontrol;
 Button btn;
     public PostClass2(ArrayList<String> groups, FragmentAnasayfa context) {
@@ -59,17 +53,28 @@ Button btn;
 btn=(Button)customView.findViewById(R.id.buttoninvitegroup);
 
         groupadi.setText(groups.get(position));
-final String swap=groups.get(position);
+ swap=groups.get(position);
      //
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAuth= FirebaseAuth.getInstance();
-                FirebaseUser user = mAuth.getCurrentUser();
-                String userEmailadd = user.getEmail().toString();
+
                 firebaseDatabase= FirebaseDatabase.getInstance();
                 myRef=firebaseDatabase.getReference();
+                mAuth= FirebaseAuth.getInstance();
+                FirebaseUser user = mAuth.getCurrentUser();
+               String uye="Üye";
+                String userEmailadd = user.getEmail().toString();
+                String uyeler="Üyeler";
+              //  String uyeSayisi=myRef.child("Groups").child(swap).child("Uye Sayisi").getKey();
+              //  int uyesayisiInt=Integer.parseInt(uyeSayisi);
+                //uyesayisiInt=uyesayisiInt+1;
+                //String uyesayisiString=Integer.toString(uyesayisiInt);
+                //myRef.child("Groups").child(swap).child("Uye Sayisi").setValue(uyesayisiString);
+
                 myRef.child("Groups").child(swap).child("Üye").setValue(userEmailadd);
                 //  myRef.child("Groups").child(uuidString).child("Yönetici").setValue(userEmail);
             }
